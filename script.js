75f07f9b006b9d2028061123721d4cc69f4da2f8
MariuszRozycki <mariansc83@icloud.com>
2021-10-07 10:22:47 +0200

    Removed url to links in social-media

--- script.js
+++ script.js
@@ -11,19 +11,19 @@ let messenger = document.querySelector('.messenger');
 let twitter = document.querySelector('.twitter');
 
 youTube.addEventListener('click', () => {
-  alert(`You have clicked icon of YouTube kanal of our shop, but website isn't ready yet. So you are transfered to YouTube home page!`);
+  alert(`You have clicked icon of YouTube kanal of our shop, but website isn't ready yet!`);
 });
 instagram.addEventListener('click', () => {
-  alert(`You have clicked icon of Instagram of our shop, but website isn't ready yet. So you are transfered to home Instagram page!`);
+  alert(`You have clicked icon of Instagram of our shop, but website isn't ready yet!`);
 });
 facebook.addEventListener('click', () => {
-  alert(`You have clicked icon of Facebook website of our shop, but website isn't ready yet. So you are transfered to Facebook home page!`);
+  alert(`You have clicked icon of Facebook website of our shop, but website isn't ready yet!`);
 });
 messenger.addEventListener('click', () => {
-  alert(`You have clicked icon of messenger of our shop, but messenger isn't connected yet. So you are transfered to Messenger home page!`);
+  alert(`You have clicked icon of messenger of our shop, but messenger isn't connected yet!`);
 });
 twitter.addEventListener('click', () => {
-  alert(`You have clicked icon of Twitter, but Twitter account isn't ready yet. So you are transfered to Twitter home page!`);
+  alert(`You have clicked icon of Twitter, but Twitter account isn't ready yet!`);
 });
 
 
