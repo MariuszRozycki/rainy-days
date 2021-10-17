796b8414bddf245f357b0eb790280e4c0e368fae
MariuszRozycki <mariansc83@icloud.com>
2021-10-17 18:01:12 +0200

    Add 4 new *.js files

---
+++ js/product-generator.js
@@ -0,0 +1,15 @@
+const productsWrapper = document.querySelector('.products-wrapper');
+console.log(productsWrapper);
+
+for (let product of products) {
+  productsWrapper.innerHTML +=
+    `<div class="item">
+  <div class="item__picture">
+    <img src="${product.image}"
+      alt="Picture of ${product.name}">
+  </div>
+  <h3 class="item__product-name">${product.name}</h3>
+  <p class="item__product-price">${product.price} nok</p>
+  <a href="#" title="Raincut Wtp" class="btn btn__show-item">Show Item</a>
+</div>`
+}
---
+++ js/womens-jackets.js
@@ -0,0 +1,36 @@
+const products = [
+  {
+    id: 1,
+    name: 'Raincut WTP',
+    image: '../images/jackets/woman-jackets/raincut-wtp.png',
+    price: 999
+  },
+
+  {
+    id: 2,
+    name: 'Sailing 5000',
+    image: '../images/jackets/woman-jackets/sailing-5000.png',
+    price: 1099
+  },
+
+  {
+    id: 3,
+    name: 'MH 9000',
+    image: '../images/jackets/woman-jackets/mh-9000.png',
+    price: 799
+  },
+
+  {
+    id: 4,
+    name: 'Tour NH500',
+    image: '../images/jackets/woman-jackets/tour-nh-500.png',
+    price: 899
+  },
+
+  {
+    id: 5,
+    name: 'Tribord 300MB',
+    image: '../images/jackets/woman-jackets/tribord-300-mb.png',
+    price: 999
+  }
+];
