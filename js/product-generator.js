0b3a4d043c251230b38ae7d4ee1519a01cc3cc78
MariuszRozycki <mariansc83@icloud.com>
2021-10-20 08:43:15 +0200

    Change pictures size

--- js/product-generator.js
+++ js/product-generator.js
@@ -3,15 +3,16 @@
 const productsWrapper = document.querySelector('.products-wrapper');
 
 for (let product of products) {
-  let productLink = `<a href="#" title="Raincut Wtp" class="btn btn__show-item">Show Item</a>`;
+  let productLink = `<a href="#" title="${product.name}" class="btn btn__show-item">Show Item</a>`;
   productsWrapper.innerHTML +=
     `<div class="item">
-  <div class="item__picture">
-    <img src="${product.image}"
-      alt="Picture of ${product.name}">
-  </div>
-  <h3 class="item__product-name">${product.name}</h3>
-  <p class="item__product-price">${product.price} nok</p>
-  ${productLink}
-</div>`
+      <div class="item__picture">
+        <img src="${product.image}" alt="Picture of ${product.name}">
+      </div>
+      <div>
+        <h3 class="item__product-name">${product.name}</h3>
+        <p class="item__product-price">${product.price} nok</p>
+        ${productLink}
+      </div>  
+  </div>`
 }
