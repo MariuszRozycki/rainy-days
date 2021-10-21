cf5e1f419717f62fad491ed53483bf2ed7189be2
MariuszRozycki <mariansc83@icloud.com>
2021-10-21 13:54:52 +0200

    Add changes in submit.js

--- js/submit.js
+++ js/submit.js
@@ -1,11 +1,13 @@
-$('#form').submit(function () {
-  if (!attributeSupported("required") || ($.browser.safari)) {
-    $("#form [required]").each(function (index) {
-      if (!$(this).val()) {
-        alert("Please fill all required fields.");
-        return false;
-      }
-    });
-  }
-  return false;
+$(document).ready(function () {
+  $('#form').submit(function () {
+    if (!attributeSupported("required") || ($.browser.safari)) {
+      $("#form [required]").each(function (index) {
+        if (!$(this).val()) {
+          alert("Please fill all required fields.");
+          return false;
+        }
+      });
+    }
+    return false;
+  });
 });
