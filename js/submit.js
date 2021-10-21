d380dee54012800f5840e2c34f0aedf00ff45e60
MariuszRozycki <mariansc83@icloud.com>
2021-10-21 13:44:50 +0200

    Add submit.js

---
+++ js/submit.js
@@ -0,0 +1,11 @@
+$('#form').submit(function () {
+  if (!attributeSupported("required") || ($.browser.safari)) {
+    $("#form [required]").each(function (index) {
+      if (!$(this).val()) {
+        alert("Please fill all required fields.");
+        return false;
+      }
+    });
+  }
+  return false;
+});
