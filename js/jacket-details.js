1bfccdd448b4e01f5bc78c8113483f9ed65f3a42
MariuszRozycki <mariansc83@icloud.com>
2022-09-16 23:12:42 +0200

    Add conditions for products types to choose-jacket-menu in jacket-details.js

--- js/jacket-details.js
+++ js/jacket-details.js
@@ -1,6 +1,3 @@
-/* container for menuDetail */
-const menuDetail = document.querySelector('.choose-jacket__menu-detail');
-
 /* container for productWrapper */
 const productWrapper = document.querySelector('.products-wrapper');
 
@@ -11,13 +8,43 @@ const productId = params.get("id");
 
 const product = products.find(({ id }) => id == productId);
 
-/* placing a name of product got from queryString */
-menuDetail.innerHTML +=
-  `<a>${product.name}</a>`
+/* choose-jacket__menu */
+const chooseJacketMenu = document.querySelector(".choose-jacket__menu");
+console.log(chooseJacketMenu);
 
-/* placing product-detail html code & rating inside .product-wrapper */
-productWrapper.innerHTML +=
-  `<div class="item">
+if (product.type === 'mens-jackets') {
+  chooseJacketMenu.innerHTML = `
+  <li><a href="../layout/our-jackets.html">Our Jackets</a></li>
+  <li><img class="menu-arrow" src="../images/menu-arrow.svg" alt="Arrow inside menu"></li>
+  <li><a href="../layout/mens-jackets.html">Men's Jackets</a></li>
+  <li><img class="menu-arrow" src="../images/menu-arrow.svg" alt="Arrow inside menu"></li>
+  <li class="choose-jacket__menu-active choose-jacket__menu-detail"><a>${product.name}</a></li>
+`;
+}
+if (product.type === 'womens-jacket') {
+  chooseJacketMenu.innerHTML = `
+  <li><a href="../layout/our-jackets.html">Our Jackets</a></li>
+  <li><img class="menu-arrow" src="../images/menu-arrow.svg" alt="Arrow inside menu"></li>
+  <li><a href="../layout/womens-jackets.html">Women's Jackets</a></li>
+  <li><img class="menu-arrow" src="../images/menu-arrow.svg" alt="Arrow inside menu"></li>
+  <li class="choose-jacket__menu-active choose-jacket__menu-detail"><a>${product.name}</a></li>
+  `;
+}
+if (product.type === 'raincoat') {
+  chooseJacketMenu.innerHTML = `
+  <li><a href="../layout/our-jackets.html">Our Jackets</a></li>
+  <li><img class="menu-arrow" src="../images/menu-arrow.svg" alt="Arrow inside menu"></li>
+  <li><a href="../layout/raincoats.html">Raincoats</a></li>
+  <li><img class="menu-arrow" src="../images/menu-arrow.svg" alt="Arrow inside menu"></li>
+  <li class="choose-jacket__menu-active choose-jacket__menu-detail"><a>${product.name}</a></li>
+  `;
+}
+
+
+function renderProduct() {
+  /* placing product-detail html code & rating inside .product-wrapper */
+  productWrapper.innerHTML +=
+    `<div class="item">
   <div class="item__picture">
     <img src="${product.image}" alt="Picture of ${product.name}">
   </div>
@@ -63,8 +90,10 @@ productWrapper.innerHTML +=
       <li>Bera sisum sit dolor</li>
     </ul>
   </div>
-  <button id="add-to-cart" class="btn">Add Item</button>
+  <button onclick="addToCart(${product.id})" id="add-to-cart" class="btn">Add Item</button>
 </div>`
+}
+renderProduct();
 
 /* container for rating */
 const ratingContainer = document.querySelector('.rating');
@@ -78,151 +107,170 @@ ratingContainer.addEventListener('click', (event) => {
   }
 });
 
-// Add product to cart code
-
-// cart Array
-let cart = [];
-
-// Add product to cart code
-const addToCartButton = document.querySelector('#add-to-cart');
-
-// container for cart-details
-const cartDetails = document.querySelector('.list-of-products');
-
-const addToCart = () => {
-  // check if product already exists
-  if (cart.some(el => el.id === product.id)) {
-    changeNumberOfUnits('plus', product.id);
-  } else {
-    // product destructure
-    cart.push({
-      ...product,
-      numberOfUnits: 1
-    });
-  }
-  updateCart();
-}
-
-// create function updateCart()
-const updateCart = () => {
-  renderCartDetails();
-  renderSubtotal();
-}
-
-addToCartButton.addEventListener('click', addToCart);
 
 
 
-// create renderCartItems()
-const renderCartDetails = () => {
 
-  cartDetails.innerHTML = "";
-  cart.forEach((item) => {
-    let productLink = `<a href="./jacket-details.html?id=${item.id}" id="${item.id}" title="${item.name}">Product: ${item.name}</a>`;
-    cartDetails.innerHTML += `
-    <thead>
-    <tr>
-      <th></th>
-      <th>Product</th>
-      <th>Size</th>
-      <th>Price</th>
-      <th class="amount">Amount</th>
-      <th></th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr class="cart-order">
-      <td>1</td>
-    </tr>
-    <tr class="cart-product-details">
-      <td><img src="${item.image}" alt="${item.name}"></td>
-      <td>${productLink}</a></td>
-      <td>Size: <a>XS</a></td>
-      <td>Price: ${item.price}</td>
-    </tr>
-    <tr class="cart-number-of-units">
-      <td><button onclick="changeNumberOfUnits('plus', ${productId})">+</button></td>
-      <td>${item.numberOfUnits}</td>
-      <td><button onclick="changeNumberOfUnits('minus', ${productId})">-</button></td>
-    </tr>
-    <tr class="remove">
-      <td><i class="fas fa-trash-alt"></i></td>
-    </tr>
-  </tbody>
-  
-  `
-  });
 
-  // removeButton
-  const removeButton = document.querySelector(".remove");
 
-  // remove item from cart
-  const removeItem = () => {
-    cart = cart.filter(item => {
-      item.id !== product.id
-    });
-    updateCart();
-  }
 
-  if (removeButton === null) {
-    return null
-  } else {
-    removeButton.addEventListener('click', removeItem);
-  }
-};
 
-// change number of units
-const changeNumberOfUnits = (action, id) => {
 
-  cart = cart.map(item => {
-    let numberOfUnits = item.numberOfUnits;
 
-    if (item.id === id) {
-      if (action === 'plus' && numberOfUnits < item.inStock) {
-        numberOfUnits++;
-      } else if (action === 'minus' && numberOfUnits > 1) {
-        numberOfUnits--;
-      }
 
-      return {
-        ...item,
-        numberOfUnits
-      }
 
-    }
-  });
 
-  updateCart();
-}
 
-// subtotal container
-const subtotal = document.querySelector(".subtotal");
-console.log("subtotal", subtotal);
 
-// cart container in nav
-const cartContainer = document.querySelector('.shopping-cart');
 
-// calculate subtotal
-const renderSubtotal = () => {
 
-  let totalPrice = 0;
-  let totalItems = 0;
 
-  cart.forEach(item => {
-    totalPrice += item.price * item.numberOfUnits;
-    totalItems += item.numberOfUnits;
-  })
 
-  subtotal.innerHTML = `
-    <tr>
-      <td>Price total:</td>
-      <td>${totalPrice.toFixed(2)} nok</td>
-      <td>(Items: ${totalItems})</td>
-    </tr>
-  `
 
-  cartContainer.innerHTML += `<div class="item-quantity">${totalItems}</div>`
-}
+
+// First version doesn't work correctly
+// // Add product to cart code
+// // cart Array
+// let cart = JSON.parse(localStorage.getItem("CART")) || [];
+// console.log("cart", cart);
+
+// // Add product to cart code
+
+// // container for cart-details
+// const cartDetails = document.querySelector('.list-of-products');
+
+// function addToCart(id) {
+//   // check if product already exists
+//   console.log("id", id);
+//   if (cart.some(item => item.id === id)) {
+//     changeNumberOfUnits('plus', product.id);
+//   } else {
+//     // product destructure & adding new property "numberOfUnits" to product
+
+//     cart.push({
+//       ...product,
+//       numberOfUnits: 1
+//     });
+//   }
+//   updateCart();
+// }
+
+// // create function updateCart()
+// const updateCart = () => {
+//   renderCartDetails();
+//   renderSubtotal();
+
+//   // save cart to local storage
+//   localStorage.setItem("CART", JSON.stringify(cart));
+// }
+
+// // create renderCartDetails()
+// function renderCartDetails() {
+
+//   // working code but with problems
+//   cartDetails.innerHTML = "";
+//   cart.forEach((item) => {
+//     let productLink = `<a href="./jacket-details.html?id=${item.id}" id="${item.id}" title="${item.name}">Product: ${item.name}</a>`;
+//     cartDetails.innerHTML += `
+//     <thead>
+//     <tr>
+//       <th></th>
+//       <th>Product</th>
+//       <th>Size</th>
+//       <th>Price</th>
+//       <th class="amount">Amount</th>
+//       <th></th>
+//     </tr>
+//   </thead>
+//   <tbody>
+//     <tr class="cart-order">
+//       <td>1</td>
+//     </tr>
+//     <tr class="cart-product-details">
+//       <td><img src="${item.image}" alt="${item.name}"></td>
+//       <td>${productLink}</a></td>
+//       <td>Size: <a>XS</a></td>
+//       <td>Price: ${item.price}</td>
+//     </tr>
+//     <tr class="cart-number-of-units">
+//       <td><button onclick="changeNumberOfUnits('plus', ${item.id})">+</button></td>
+//       <td>${item.numberOfUnits}</td>
+//       <td><button onclick="changeNumberOfUnits('minus', ${item.id})">-</button></td>
+//     </tr>
+//     <tr onclick="removeItem(${item.id})" class="remove">
+//       <td><i class="fas fa-trash-alt"></i></td>
+//     </tr>
+//   </tbody>
+//   `
+//   });
+// };
+
+// // remove item from cart
+// const removeItem = (id) => {
+//   console.log("id", id);
+
+
+//   cart = cart.filter(item => {
+//     console.log("id", item.id);
+//     id === item.id;
+//   });
+
+//   updateCart();
+// }
+
+// // change number of units
+// const changeNumberOfUnits = (action) => {
+//   console.log("cart in changeNumberOfUnits", cart);
+
+//   cart = cart.map(item => {
+//     let numberOfUnits = item.numberOfUnits;
+
+//     if (item.id === item.id) {
+//       if (action === 'plus' && numberOfUnits < item.inStock) {
+//         numberOfUnits++;
+//       } else if (action === 'minus' && numberOfUnits > 1) {
+//         numberOfUnits--;
+//       }
+
+//       return {
+//         ...item,
+//         numberOfUnits
+//       }
+
+//     }
+//   });
+//   updateCart();
+// }
+
+// // subtotal container
+// const subtotal = document.querySelector(".subtotal");
+
+// // cart container in nav
+// const cartContainer = document.querySelector('.shopping-cart');
+
+// // calculate subtotal
+// const renderSubtotal = () => {
+
+//   let totalPrice = 0;
+//   let totalItems = 0;
+
+//   cart.forEach(item => {
+//     totalPrice += item.price * item.numberOfUnits;
+//     totalItems += item.numberOfUnits;
+//   })
+
+//   subtotal.innerHTML = `
+//     <tr>
+//       <td>Price total:</td>
+//       <td>${totalPrice.toFixed(2)} nok</td>
+//       <td>(Items: ${totalItems})</td>
+//     </tr>
+//   `
+
+//   cartContainer.innerHTML += `<div class="item-quantity">${totalItems}</div>`
+// }
+
+// updateCart();
 
 
 
